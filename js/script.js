d430aba49d42d097231e0d666d64b371d5696bb2
ibrahimfares511 <ibrahimfares511@gmail.com>
2024-08-24 23:56:34 +0300

    Fix Responsive Design

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ document.addEventListener("scroll", function () {
   const sections = document.querySelectorAll("section");
   sections.forEach((section) => {
     const rect = section.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 300 && rect.bottom > 0) {
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
       section.classList.add("visible");
     } else {
       section.classList.remove("visible");
